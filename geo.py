5dd3e52d530494fa26eed593e3872d3c535df738
HCLivess <admin@bismuth.cz>
2019-10-17 16:02:10 +0200

    clean entry after 24 hours

--- geo.py
+++ geo.py
@@ -14,12 +14,19 @@ class ThreadedClient(threading.Thread):
     def run(self):
        while True:
            updater.update()
+           updater.clean()
            print("Waiting 5 minutes")
            time.sleep(360)
 
 class Updater():
     def __init__(self):
         self.data = []
+
+    def clean(self):
+        for key in self.data:
+            if time.time() - int(key['added']) > 86400:
+                self.data.remove(key)
+                print(f"Removed {key}")
 
     def update(self):
         self.connection = rpcconnections.Connection(("127.0.0.1", 5658))
@@ -36,6 +43,7 @@ class Updater():
                 print(ip)
                 coordinates = json.loads(requests.request("GET", f"http://api.ipstack.com/{ip}?access_key={api_key}").text)
                 coordinates['ip'] = ip
+                coordinates['added'] = time.time()
                 if coordinates['latitude'] and coordinates['longitude']:
                     self.data.append(coordinates)
 
